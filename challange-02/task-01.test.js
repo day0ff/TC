d88b74a8cf416d4e79eae4fd452eeb4aab007fe5
Viarshadau Dzianis <dzianis_viarshadau@senla.eu>
2020-12-06 19:00:43 +0300

    Implement testing examples for tasks 1 and 2.

---
+++ challange-02/task-01.test.js
@@ -0,0 +1,5 @@
+import cube from './task-01.js';
+
+test('cube', () => {
+    expect(cube(3)).toBe(27);
+});
---
+++ challange-02/task-02.test.js
@@ -0,0 +1,5 @@
+import dayOfWeek from './task-02.js';
+
+test('cube', () => {
+    expect(dayOfWeek(1)).toBe('Monday');
+});
