968704427fe8022d8bb1325828c3d73972312e35
Viarshadau Dzianis <dzianis_viarshadau@senla.eu>
2020-12-06 22:35:35 +0300

    Implement challenge 2 task 3. Add tests.

---
+++ challange-02/task-03.js
@@ -0,0 +1,3 @@
+export default function factorial(x) {
+	// write code here
+}
---
+++ challange-02/task-03.test.js
@@ -0,0 +1,37 @@
+import factorial from './task-03.js';
+
+describe('Factorial function: ', () => {
+
+    test('should retrun 0 (factorial 1!)', () => {
+        expect(factorial(0)).toBe(1);
+    });
+
+    test('should retrun 1 (factorial 1!)', () => {
+        expect(factorial(1)).toBe(1);
+    });
+
+    test('should retrun 2 (factorial 2!)', () => {
+        expect(factorial(2)).toBe(2);
+    });
+
+    test('should retrun 6 (factorial 3!)', () => {
+        expect(factorial(3)).toBe(6);
+    });
+
+    test('should retrun 5040 (factorial 7!)', () => {
+        expect(factorial(7)).toBe(5040);
+    });
+
+    test('should retrun NaN (factorial string "a"!)', () => {
+        expect(factorial('a')).toBeNaN();
+    });
+
+    test('should retrun 1 (factorial string "1"!)', () => {
+        expect(factorial('1')).toBe(1);
+    });
+
+    test('should retrun 720 (factorial string "6"!)', () => {
+        expect(factorial('6')).toBe(720);
+    });
+
+});
